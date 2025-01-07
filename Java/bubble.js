34e1a2f59faedc0c39756a3bb1bcdef212b62142
Kasza Lehel <gamerleaguelol4@gmail.com>
2025-01-07 15:58:07 +0100

    20
    20

--- Java/bubble.js
+++ Java/bubble.js
@@ -4,12 +4,12 @@ var tgX = 0;
 var tgY = 0;
 
 document.addEventListener('DOMContentLoaded', function () {
-    var interBubble = document.querySelector('interactive');
+    var interBubble = document.querySelector('.interactive');
 
     function move() {
         // Növeljük az elmozdulás mértékét, hogy gyorsabban kövesse az egeret
-        curX += (tgX - curX) / 10; // Az 1/10 gyorsabb mozgást biztosít
-        curY += (tgY - curY) / 10; // Az 1/10 gyorsabb mozgást biztosít
+        curX += (tgX - curX) / 20; // Az 1/10 gyorsabb mozgást biztosít
+        curY += (tgY - curY) / 20; // Az 1/10 gyorsabb mozgást biztosít
         interBubble.style.transform = 'translate(' + Math.round(curX) + 'px, ' + Math.round(curY) + 'px)';
 
         // Request animation frame újraindítása
