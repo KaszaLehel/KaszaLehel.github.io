c60dec28fa580917f45b9f3b5a1b84b349b672ea
Kasza Lehel <gamerleaguelol4@gmail.com>
2025-01-09 21:01:08 +0100

    V-6.1
    menu navigator

--- Java/index.js
+++ Java/index.js
@@ -21,4 +21,112 @@ document.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('resize', toggleButtonVisibility);
     toggleButtonVisibility();
+
+
+
+const menuItems = document.querySelectorAll('.menu-item');
+const menu = document.querySelector('#menu');
+const sections = document.querySelectorAll('section'); 
+const personSection = document.querySelector('#person');
+
+let isMouseTrackingEnabled = false;
+
+
+
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        
+        if (entry.isIntersecting) {
+            //console.log(entry.target.id + ' szekció látható!');
+
+            if (entry.target.id === 'projects') {
+                menuItems.forEach(item => {
+                    item.classList.remove('white');
+                    item.classList.add('black');
+                });
+            }else {
+                menuItems.forEach(item => {
+                    item.classList.remove('black');
+                    item.classList.add('white');
+                });
+            }
+            
+            if (entry.target.id === 'person') 
+            {
+                menuItems.forEach(item => item.classList.remove('visible'));
+                //console.log('LÁTHATATLAN');
+                isMouseTrackingEnabled = true;
+            } 
+            else 
+            {
+                menuItems.forEach(item => item.classList.add('visible'));
+                isMouseTrackingEnabled = false;
+            }
+        }
+    });
+}, {
+    threshold: 0.5 
+    });
+
+    sections.forEach(section => {
+    observer.observe(section);
 });
+
+    
+    menuItems.forEach(item => {
+        item.addEventListener('click', function () {
+            const targetSection = document.getElementById(item.getAttribute('data-target'));
+            if (targetSection) {
+                targetSection.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            }
+        });
+    });
+
+
+
+
+
+const distanceThreshold = 100;
+function isMouseNearElement(e) {
+    const rect = menu.getBoundingClientRect();
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    if (
+        mouseX >= rect.left - distanceThreshold &&
+        mouseX <= rect.right + distanceThreshold &&
+        mouseY >= rect.top - distanceThreshold &&
+        mouseY <= rect.bottom + distanceThreshold
+    ) {
+        return true;  
+    }
+    return false;  
+}
+
+document.addEventListener('mousemove', (e) => {
+
+    if (isMouseTrackingEnabled) {
+        if (isMouseNearElement(e)) {
+            //console.log('Az egér közel van a div elemhez!');
+            menuItems.forEach(item => item.classList.add('visible'));
+        } else {
+            //console.log('Az egér nincs közel a div elemhez.');
+            menuItems.forEach(item => item.classList.remove('visible'));
+        }
+    }
+});
+
+
+
+});
+
+
+
+
+
+
+
